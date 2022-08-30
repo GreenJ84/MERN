4be91291e4da63963d94c260fdb628ccf6b6b583
Jesse Greenough <jesse.greenough84@gmail.com>
2022-08-29 17:13:33 -0700

    Added first React compenent

---
+++ React/first_jsx/src/components/SomeClassComponent.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+    
+    
+class SomeClassComponent extends Component {
+    render() {
+        return <div>{this.props.someText} This is our first class component.</div>
+    }
+}
+    
+export default SomeClassComponent;
