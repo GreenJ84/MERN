52b6ee4f2e659c344841af64722e2ea334a85bef
Jesse Greenough <jesse.greenough84@gmail.com>
2022-08-30 14:17:02 -0700

    Commit minor changes

--- React/first_jsx/src/App.js
+++ React/first_jsx/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
-import PersonCard from './components/PersonCard';
-
-import SomeClassComponent from './components/SomeClassComponent';
 
 function App() {
   return (
     <div className="App">
       <>
         <h1>Hello Dojo</h1>
+      </>
       <table>
         <tr><th>Things I need to do: </th></tr>
         <tr>
@@ -20,14 +18,6 @@ function App() {
           </ul>
         </tr>
       </table>
-      <SomeClassComponent someText={"Hello World,"}/>
-      <hr/>
-      <PersonCard firstName={"Jane"} lastName ={"Doe"} age={45} hairColor={"black"}/>
-      <PersonCard firstName={"John"} lastName ={"Smith"} age={88} hairColor={"brown"}/>
-      <PersonCard firstName={"Millard"} lastName ={"Fillmore"} age={50} hairColor={"blonde"}/>
-      <PersonCard firstName={"Maria"} lastName ={"Smith"} age={62} hairColor={"gray"}/>
-
-      </>
     </div>
   );
 }
