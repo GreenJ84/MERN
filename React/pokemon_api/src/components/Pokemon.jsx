fc9b8ee4d617307dcb9cd9255a523e82d73eb2b0
Jesse Greenough <jesse.greenough84@gmail.com>
2022-09-05 15:31:48 -0700

    Completed assignment. Pokemon API Call

---
+++ React/pokemon_api/src/components/Pokemon.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect} from 'react';
+
+const Pokemon = () => {
+    const [pokemon, setPokemon] = useState([]);
+
+    const tryThis = () => {
+        fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=807')
+            .then(response => response.json())
+            .then(response => setPokemon(response.results))
+    }
+
+    return (
+        <div>
+            <button onClick={tryThis}>Fetch Pokemon</button>
+            {pokemon.map((poke, idx) => {
+                return <li key={idx}>{poke.name}</li>
+            })}
+        </div>
+    )
+} 
+export default Pokemon;
