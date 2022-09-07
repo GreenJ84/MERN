af9cdd75174c5c8fde230d0a16e9b2987954659d
Jesse Greenough <jesse.greenough84@gmail.com>
2022-09-07 12:25:15 -0700

    Completed routing practice app.

--- React/routing_practice/src/App.js
+++ React/routing_practice/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route, useParams } from "react-router-dom";
 import './App.css';
+
+const Welcome = () => {
+  return (<h1>Welcome!</h1>);
+}
+
+const Display = (props) => {
+  const {item} = useParams();
+
+  if (Number.isNaN(+item)){
+    return (
+    <h2>The Word is: {item}!</h2>);
+  } else{
+    return (
+      <h2>The Number is: {item}!</h2>);
+  }
+  
+}
+
+const Style = (props) => {
+  const {item, text, bgc} = useParams();
+  const styleStyle ={
+    background: bgc,
+    color: text
+  }
+  return (
+    <h2 style={ styleStyle }>The Word is: {item}!</h2>);
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Routing Example</h1>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/:item" element={<Display />} />
+        <Route path="/:item/:text/:bgc" element={<Style />} />
+      </Routes>
     </div>
   );
 }
