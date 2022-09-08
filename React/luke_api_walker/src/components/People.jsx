661c9dd9b7ce34178b4db509965d189dd89d1ac9
Jesse Greenough <jesse.greenough84@gmail.com>
2022-09-07 17:14:32 -0700

    Built out people type display component

---
+++ React/luke_api_walker/src/components/People.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react'
+
+const People = (props) => {
+    const results = props.results;
+    const [homeworld, setHomeworld] = useState(results.homeworld);
+
+    fetch(homeworld)
+        .then(response => response.json)
+        .then(response => setHomeworld(response))
+        
+    return (
+        <>
+        <h1>{results.name}</h1>
+        <table>
+            <tr>
+                <th>
+                    Height: 
+                </th>
+                <td>
+                    {results.height} cm
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    Mass: 
+                </th>
+                <td>
+                    {results.mass} kg
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    Hair Color:
+                </th>
+                <td>
+                    {results.hair_color}
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    Skin Color: 
+                </th>
+                <td>
+                    {results.skin_color}
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    Eye Color: 
+                </th>
+                <td>
+                    {results.eye_color}
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    Birth Year: 
+                </th>
+                <td>
+                    {results.birth_year}
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    Gender: 
+                </th>
+                <td>
+                    {results.gender}
+                </td>
+            </tr>
+            <tr>
+                <th>
+
+                </th>
+                <td>
+                    {homeworld.name} (Population: {homeworld.population})
+                </td>
+            </tr>
+        </table>
+        </>
+    )
+}
+
+export default People;
