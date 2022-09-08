ac86a426fb02add44b652e023c0bd04eb11e1cd0
Jesse Greenough <jesse.greenough84@gmail.com>
2022-09-07 21:02:54 -0700

    Completed coinFlip assignment

--- React/coin_flipping/src/App.js
+++ React/coin_flipping/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+  function tossCoin() {
+    return Math.random() > 0.5 ? "heads" : "tails";
+  }
+
+  function fiveHeadsSync() {
+    let headsCount = 0;
+    let attempts = 0;
+    while(headsCount < 5) {
+        attempts++;
+        let result = tossCoin();
+        console.log(`${result} was flipped`);
+        if(result === "heads") {
+            headsCount++;
+        } else {
+            headsCount = 0;
+        }
+        if (attempts > 100){
+          return null;
+        }
+      }
+    return `It took ${attempts} tries to flip five "heads"`;
+  }
+
+  let attempts = 0;
+
+  function fiveHeads() {
+    return new Promise( (resolve, reject) => {
+        attempts = fiveHeadsSync();
+        if (attempts == null){
+          attempts = "It has been over 100 attempts with no success!";
+          reject(attempts);
+        }
+        else { resolve( attempts )}
+    });
+}
+  fiveHeads()
+      .then( res => console.log(res+"1") )
+      .catch( err => console.log(err+"2") );
+  console.log( "When does this run now?" );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{attempts}</h1>
     </div>
   );
 }
