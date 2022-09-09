f629c2848726335b1530aeb02ff5186f37bfb423
Jesse Greenough <jesse.greenough84@gmail.com>
2022-09-09 15:17:39 -0700

    completed mongoose backend connection

--- MERN/HelloMongoose/server/controllers/user.controller.js
+++ MERN/HelloMongoose/server/controllers/user.controller.js
@@ -13,13 +13,14 @@ module.exports.findOneSingleUser = (req, res) => {
 };
 
 module.exports.createNewUser = (req, res) => {
+  console.log(req.body);
   User.create(req.body)
     .then(newlyCreatedUser => res.json({ user: newlyCreatedUser }))
     .catch(err => res.json({ message: "Something went wrong", error: err }));
 };
 
 module.exports.updateExistingUser = (req, res) => {
-  User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+  User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true, runValidators: true })
     .then(updatedUser => res.json({ user: updatedUser }))
     .catch(err => res.json({ message: "Something went wrong", error: err }));
 };
--- MERN/HelloMongoose/server/models/user.model.js
+++ MERN/HelloMongoose/server/models/user.model.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-	name: String,
-	age: Number
-});
+	name: {
+		type: String,
+		required: [true, "Please make sure to fill out name!"],
+		minLength: [4, "Name must be 4 or more characters!"]
+	},
+	age: {
+		type: Number,
+		required: [true, "Please make sure to fill out age!"],
+		min: [18, "Age must be equal to 18 or greater"]
+	}},
+	{timestamps: true});
 
 const User = mongoose.model("User", UserSchema);
 
