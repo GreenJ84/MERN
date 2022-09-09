54b181528d11a31a7449d0670be38f7eac3004bf
Jesse Greenough <jesse.greenough84@gmail.com>
2022-09-09 15:18:09 -0700

    App clean up

--- MERN/HelloMongoose/server.js
+++ MERN/HelloMongoose/server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
-// This will fire our mongoose.connect statement to initialize our database connection
+
 app.use(express.json(), express.urlencoded({ extended: true }));
 
 require("./server/config/mongoose.config");
 require("./server/routes/user.routes")(app);
-
-
 
 app.listen(8000, () => console.log("The server is all fired up on port 8000"));
