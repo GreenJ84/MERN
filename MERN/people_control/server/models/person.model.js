4e32008a48d4623b17f8f3fa199b86f4f90356ad
Jesse Greenough <jesse.greenough84@gmail.com>
2022-09-12 10:05:32 -0700

    Changed app name project -> product

--- MERN/people_control/server/models/person.model.js
+++ MERN/people_control/server/models/person.model.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose')
 const PersonSchema = new mongoose.Schema({
     firstName: {
         type: String,
-        required: true,
-        min: 3
+        required: [true, "First name is Required!"],
+        min: [3, "First Name must be more than 4 characters."]
     },
     lastName: {
         type: String,
-        required: true,
-        min: 4
+        required: [true, "Last name is Required!"],
+        min: [4, "Last Name must be more than 4 characters."]
     }
 }, {timestamps: true});
 
---
+++ MERN/product_manager/server/models/product.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "Product Title is required!"],
+        min: [4, "Title must be longer than 3 characters."]
+    },
+    price: {
+        type: Number,
+        required: [true, "Thr Product must have a price!"],
+        min: [.5, "Price must be above 50 cents to be sold here."]
+    },
+    description: {
+        type: String,
+        required: [true, "Consumers must know what they are getting!"],
+        min: [8, "You really cant describe a product with less than 8 characters...."]
+    }
+});
+const Product = mongoose.model("Product", ProductSchema);
+export default Product;
